339cd8aefad099fe131ee0fa235fb73daed6b99e
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-02-14 11:48:41 +0100

    11-48

--- harmadik.php
+++ harmadik.php
@@ -30,6 +30,86 @@ for ($i = 1;$i <= 30;$i++){
     print $i;
     }
 }
+//vegigmegíunk a tombon
+for($i = 0;$i < count($tomb);$i++){
+    if($i%2==0){
+        print $tomb[$i] . '<br />';
+    }
+    
+}
+//foreach
+foreach ($asszTomb as $kulcs => $elem){
+    print $kulcs.': '. $elem.'<br />';
+}
+
+print_r($asszTomb);
+print '<br />';
+//fuggveny
+
+//paraméter alapján ,paraméter nélküli
+//értékvisszaadó ,végrehajtó
+
+
+//parameter nelkuli,végre hajtó
+function kettoMegKetto(){
+    print '4'.'<br />';
+}
+
+kettoMegKetto();
+//paraméteres,végrehajtó
+function osszead($a,$b){
+    print $a+$b.'<br /> ';
+}
+osszead(2,8);
+
+kettoMegKetto();
+//paraméteres,értékvisszaadó
+function AmegB($a,$b){
+    return $a+$b;
+}
+
+$szam=AmegB(10,1);
+print $szam.'<br />';
+print AmegB(10,1).'<br />';
+
+
+//jel == *,+,-,/
+function szamologep($a,$b,$jel){
+
+}
+
+
+$szoveg='almale';
+
+function forditas($szoveg){
+    for($i = strlen($szoveg)-1; $i>=0 ;$i--){
+        print $szoveg[$i];
+    }
+}
+print forditas($szoveg).'<br />';
+
+$p='indul a gorog aludni';
+
+$p = str_replace(' ','',$p);
+print $p .'<br />';
+
+
+$palindrom=true;
+for ($i = 0;$i < strlen($p);$i++){
+    $hatso = strlen($p) - ($i+1);
+    
+    if($p[$i] != $p[$hatso]){
+        $palindrom=false;
+        break;
+    }
+}
+if($palindrom){
+    print 'Palindrom <br />';
+}
+else{
+    print 'Nem Palindrom <br />';
+}
+
 
 
 ?>
